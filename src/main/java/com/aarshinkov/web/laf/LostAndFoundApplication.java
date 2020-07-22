052185c6f1a1cf02cf45ee8a01c495e01d1a2d96
aarshinkov <a.arshinkov97@gmail.com>
2020-07-22 20:24:17 +0300

    Added project to repository

---
+++ src/main/java/com/aarshinkov/web/laf/LostAndFoundApplication.java
@@ -0,0 +1,13 @@
+package com.aarshinkov.web.laf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostAndFoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostAndFoundApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/aarshinkov/web/laf/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.aarshinkov.web.laf;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LostAndFoundApplication.class);
+	}
+
+}
